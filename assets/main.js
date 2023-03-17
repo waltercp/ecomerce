34cbbdcd02ca9dab1f18d7f3cc0aa0c9f91ebfe8
waltercp <walter_cp_96@hotmail.com>
2023-03-17 10:03:49 -0600

    ecomerce

--- assets/main.js
+++ assets/main.js
@@ -21,12 +21,11 @@ function printProducts(db) {
 
     for (const product of db.products) {
         const buttonAdd = product.quantity
-            ? `<i class='bx bx-plus'></i>
+            ? `<i class='bx bx-plus'   id='${product.id}' ></i>
             <span class="product_info--stock">stock: ${product.quantity}</span>`
             : "<span class='product__soldout'>sold out</span>";
-
         html += `
-        <div class="product ${product.category}">
+        <div class="product shirt ${product.category}">
             <div class="product__img">
                 <img src="${product.image}" alt="imagen"/>
             </div>
@@ -117,8 +116,8 @@ function modalProduct(db) {
             const productFind = db.products.find(
                 (product) => product.id === id);
 
-
-            html += `
+        
+                html += `
                 <div class="contentProduct">
                 <i class="bx bxs-x-circle closeModal"></i>
                 <div class="contentProduct__img">
@@ -135,17 +134,17 @@ function modalProduct(db) {
             </div>
             `
         }
-
+        
         bodyHTML.innerHTML = html
         console.log(bodyHTML)
-        const iconMonHTML = document.querySelector(".bxs-x-circle")
+         const iconMonHTML = document.querySelector(".bxs-x-circle")
         iconMonHTML.addEventListener("click", function () {
             bodyHTML.classList.remove("modalProduct__show");
             location.reload();
+    
+    })
 
-        })
-
-
+    
     });
 
 
@@ -344,8 +343,6 @@ function handlePrintAmountProducts(db) {
 }
 
 
-
-
 async function main() {
 
     const db = {
@@ -355,16 +352,6 @@ async function main() {
         cart: JSON.parse(window.localStorage.getItem('cart')) || {},
     };
 
-    const filterSHTML = document.querySelectorAll(".content_filter .filter");
-
-    filterSHTML.forEach((filter) => {
-        filter.addEventListener("click", (e) => {
-            filterSHTML.forEach((filter) =>
-                filter.classList.remove("filter__active mixitup-control-active")
-            );
-            e.target.classList.add("filter__active mixitup-control-active");
-        });
-    });
 
     printProducts(db);
     handleShowCart();
@@ -378,7 +365,6 @@ async function main() {
     printTotal(db);
     handleTotal(db);
     handlePrintAmountProducts(db)
-
 
 
     mixitup(".products", {
